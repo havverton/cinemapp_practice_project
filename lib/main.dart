4ee0c9c8ee0c6266bf07c04702624dd11ee38cd3
havverton <havverton@gmail.com>
2021-04-27 01:01:48 +0300

    add favorites page, pre pagination

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'file:///D:/Flutter_Projects/cinemapp_practice_project/lib/pages/FilmPage.dart';
+import 'package:cinemapp_practice_project/pages/FavoritesPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +12,51 @@ class Cinemapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(textTheme: Typography.whiteRedmond),
-      home: Container(
-          margin: EdgeInsets.symmetric(horizontal: 7.0),
+      home: MainScaffold()
+    );
+  }
+}
+
+class MainScaffold extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _MainScaffoldState();
+}
+
+class _MainScaffoldState extends State<MainScaffold>{
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF191926),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFF191926),
+        unselectedItemColor: Colors.white24,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Movies',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+            label: 'Profile',
+          ),
+        ],
+        //currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xFFFF3365),
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+
+      ),
+      body: Container(
           color: Color(0xFF191926),
           child: Column(
             children: [
@@ -20,54 +64,17 @@ class Cinemapp extends StatelessWidget {
                 width: double.infinity,
                 height: 50,
                 child: TextButton(
-                  onPressed: () => {print("ololo")},
-                  child: Text("123"),
+                  onPressed: () => {},
+                  child: Text("${_selectedIndex == 0 ? '123' : '4321'}"),
                 ),
               ),
               Expanded(
-                child: FilmPage(),
-                /*child: GridView.count(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.58,
-                    children: [
-                      MovieCardWidget2(movie),
-
-                    ],
-
-                  ),*/
+                child: _selectedIndex == 0 ? FilmPage() : FavoritesPage(),
               )
             ],
           )
-          //child: MovieCardWidget(),
-          /*child: GridView.extent (
-        maxCrossAxisExtent: 200,
-
-          children: [
-            Column(
-              children: [
-                MovieCardWidget(),
-                MovieCardWidget(),
-              ],
-            ),
-            Column(
-              children: [
-                MovieCardWidget(),
-                MovieCardWidget(),
-              ],
-            ),
-            Container(
-              color: Colors.red
-            ),
-            MovieCardWidget(),
-            MovieCardWidget(),
-            MovieCardWidget(),
-
-            MovieCardWidget(),
-            MovieCardWidget(),
-            MovieCardWidget(),
-          ],
-        ),*/
-          ),
+      ),
     );
   }
+
 }
---
+++ lib/pages/FavoritesPage.dart
@@ -0,0 +1,84 @@
+
+import 'package:cinemapp_practice_project/MovieCardWidget.dart';
+import 'package:cinemapp_practice_project/models/MovieModel.dart';
+import 'package:cinemapp_practice_project/models/MovieProvider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cinemapp_practice_project/db/favorites_db.dart' as favDB;
+import 'package:sqflite/sqflite.dart';
+
+
+
+
+class FavoritesPage extends StatefulWidget {
+  @override
+  _FavoritesPageState createState() => _FavoritesPageState();
+}
+class _FavoritesPageState extends State<FavoritesPage>{
+
+  final database = favDB.openFavoritesDB();
+  Future<List<Movie>> testtest() async{
+  var test = favDB.readDB(database);
+  return  test;
+}
+
+  /*void storeMovie(List<Movie> movies) {
+    popularDB.insert(movies, database);
+  }*/
+  @override
+  Widget build(BuildContext context) =>
+      Scaffold(
+        body: Container(
+          padding: EdgeInsets.only(top: 5),
+          color: Color(0xFF191926),
+          child: FutureBuilder<List<Movie>>(
+            future: testtest(),
+            //future: MovieProvider().getPopular(),
+            builder: (context, snapshot) {
+              final movies = snapshot.data;
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return Center(child: CircularProgressIndicator());
+                case ConnectionState.none:
+                  print("no data");
+                  break;
+                case ConnectionState.active:
+
+                  print("no data1");
+                  break;
+
+                case ConnectionState.done:
+                  if (!snapshot.hasData) {
+                    print("YES YES YES ${snapshot.data}");
+
+                    return Text("No data");
+                  } else {
+                    print("${movies.length}");
+                    //storeMovie(movies);
+                    //read(database);
+                    return buildMovies(movies);
+                  }
+              }
+              return Center(child: CircularProgressIndicator());
+            },
+          ),
+        ),
+      );
+
+  Widget buildMovies(List<Movie> movies) =>
+      GridView.builder(
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            //storeMovie(movie);
+            return MovieCardWidget(movie);
+          },
+
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.635,
+            mainAxisSpacing: 15.0,
+          ),
+          itemCount: movies.length,
+          physics: BouncingScrollPhysics());
+
+}
