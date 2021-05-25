21fb5979b4cb19b9db37842d84b9294f19803076
havverton <havverton@gmail.com>
2021-05-26 02:23:51 +0300

    Cast in details page migrate to BLoC pattern

---
+++ lib/BLoC/movie_cast_bloc.dart
@@ -0,0 +1,42 @@
+import 'package:cinemapp_practice_project/models/CreditsModel.dart';
+import 'package:cinemapp_practice_project/services/MovieRepository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class MovieCastState {
+  late List<Cast> actors;
+}
+
+class UnknownCastState extends MovieCastState{}
+class LoadingCastState extends MovieCastState{}
+class LoadedCastState extends MovieCastState{
+  List<Cast> actors;
+  LoadedCastState({required this.actors});
+}
+class ErrorCastState extends MovieCastState{}
+
+abstract class MovieCastEvent{}
+
+class LoadCastEvent  extends MovieCastEvent{
+  int movieID;
+  LoadCastEvent({required this.movieID});
+}
+
+class MovieCastBLoC extends Bloc<MovieCastEvent, MovieCastState> {
+  final MovieRepository movieRepository;
+  MovieCastBLoC(this.movieRepository) : super(UnknownCastState());
+
+  @override
+  Stream<MovieCastState> mapEventToState(MovieCastEvent event) async*{
+    if(event is LoadCastEvent){
+      yield LoadingCastState();
+      try{
+        List<Cast> cast = await movieRepository.getActors(event.movieID);
+        yield LoadedCastState(actors: cast);
+      }catch(err){
+        print("${err.toString()}");
+        yield ErrorCastState();
+      }
+    }
+  }
+}
+
---
+++ lib/widgets/MovieCastWidget.dart
@@ -0,0 +1,105 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cinemapp_practice_project/BLoC/movie_cast_bloc.dart';
+import 'package:cinemapp_practice_project/models/CreditsModel.dart';
+import 'package:cinemapp_practice_project/services/MovieRepository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MovieCastWidget extends StatefulWidget {
+  int movieID;
+
+  MovieCastWidget(this.movieID);
+
+  @override
+  _MovieCastWidgetState createState() => _MovieCastWidgetState(this.movieID);
+}
+
+class _MovieCastWidgetState extends State<MovieCastWidget> {
+  int movieID;
+
+  _MovieCastWidgetState(this.movieID);
+
+  MovieRepository movieRepository = MovieRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (BuildContext newContext) => MovieCastBLoC(movieRepository),
+        child: Builder(builder: (providerContext) {
+          BlocProvider.of<MovieCastBLoC>(
+              providerContext)
+              .add(LoadCastEvent(movieID: movieID));
+          return BlocBuilder<MovieCastBLoC, MovieCastState>(
+              builder: (context, state) {
+                if (state is LoadingCastState) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (state is LoadedCastState) {
+                  List<Cast> actors = state.actors;
+                  return GridView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: actors.length,
+                    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                        maxCrossAxisExtent: 250,
+
+                        childAspectRatio: 1.75,
+                        crossAxisSpacing: 5,
+                        mainAxisSpacing: 8),
+                    itemBuilder: (context, index) {
+                      final actor = actors[index];
+                      return ActorsCardWidget(actor);
+                    },
+                  );
+                }else if(state is ErrorCastState){
+                  return Center(child: Text("Error"));
+                }
+                else {
+                  return Center(child: Text("Ololo"));
+                }
+              });
+        }));
+  }
+}
+
+class ActorsCardWidget extends StatelessWidget {
+  Cast actor;
+
+  ActorsCardWidget(this.actor);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      //mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        AspectRatio(
+          aspectRatio:1,
+          child: FittedBox(
+            clipBehavior: Clip.hardEdge,
+            fit: BoxFit.cover,
+          child: CachedNetworkImage(
+            imageUrl: "https://image.tmdb.org/t/p/w185${actor
+                .profilePath}",
+            placeholder: (context, url) => Container(),
+            errorWidget: (context, url, error) =>
+                Icon(Icons.account_circle_rounded),
+          ),
+          ),
+        ),
+        Container(
+
+          margin: EdgeInsets.symmetric(vertical: 6.0),
+          child: Column(
+              children: [
+                Text("${actor.name}",textAlign: TextAlign.center,),
+                Text("as ${actor.character}" ,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize:  8.0,
+                    color: Colors.blueGrey,
+                  ),)
+              ]),
+        )
+      ],
+    );
+  }
+}
